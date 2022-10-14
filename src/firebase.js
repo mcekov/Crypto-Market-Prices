85339291595585d1bc143040d80a1ab2f71d709b
Momchil Tsekov <momchil.tsekov@comstream.se>
2022-10-14 09:38:34 +0300

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFsiiIhqyk0gk_s7KCjgKap6UW1wURJRk',
-  authDomain: 'crypto-market-prices.firebaseapp.com',
-  projectId: 'crypto-market-prices',
-  storageBucket: 'crypto-market-prices.appspot.com',
-  messagingSenderId: '1010955773824',
-  appId: '1:1010955773824:web:d6494ebd68c1678ff84365',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: REACT_APP_FIREBASE_ID,
 }
 
 // Initialize Firebase
