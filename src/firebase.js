af5cd30e80d3593caab6ab3ac7752c8b4053bb9d
Momchil Tsekov <momchil.tsekov@comstream.se>
2022-10-14 09:27:56 +0300

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFsiiIhqyk0gk_s7KCjgKap6UW1wURJRk',
+  authDomain: 'crypto-market-prices.firebaseapp.com',
+  projectId: 'crypto-market-prices',
+  storageBucket: 'crypto-market-prices.appspot.com',
+  messagingSenderId: '1010955773824',
+  appId: '1:1010955773824:web:d6494ebd68c1678ff84365',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
